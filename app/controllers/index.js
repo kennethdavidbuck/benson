db37305d2d3d68c08ee1f90b238cf737930fd385
Kenneth Buck <kenneth@nestedsphere.com>
2015-12-20 23:20:46 -0500

    dry-ed up code.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
-const {getProperties} = Ember;
+const {computed, getProperties} = Ember;
 
 const sizes = ['large', 'wide', 'square', 'square', 'square', 'square', 'wide',
 	'large', 'wide', 'square', 'square', 'square', 'wide', 'square'];
 
 export default Ember.Controller.extend({
-	tiles: Ember.computed.map('model', function (post, index) {
+	tiles: computed.map('model', function (post, index) {
 		const tile = getProperties(post, 'id', 'title');
 		tile.size = sizes[index % sizes.length];
 		return tile;
--- app/controllers/posts/index.js
+++ app/controllers/posts/index.js
@@ -1,12 +1,9 @@
 import Ember from 'ember';
 
-const {getProperties} = Ember;
-
-const sizes = ['large', 'wide', 'square', 'square', 'square', 'square', 'wide',
-	'large', 'wide', 'square', 'square', 'square', 'wide', 'square'];
+const {computed, getProperties} = Ember;
 
 export default Ember.Controller.extend({
-	tiles: Ember.computed.map('model', function (post, index) {
+	tiles: computed.map('model', function (post) {
 		return getProperties(post, 'id', 'title');
 	})
 });
