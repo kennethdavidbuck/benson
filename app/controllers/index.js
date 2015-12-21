037f5ff64e9394353f0c2fdd0e8017df792e1759
Kenneth Buck <kenneth@nestedsphere.com>
2015-12-20 20:59:41 -0500

    created tile size mapping

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,7 +4,7 @@ const sizes = ['large', 'wide', 'square', 'square', 'square', 'square', 'wide',
 	'large', 'wide', 'square', 'square', 'square', 'wide', 'square'];
 
 export default Ember.Controller.extend({
-	postTiles: Ember.computed.map('model', function (post, index) {
+	tiles: Ember.computed.map('model', function (post, index) {
 		return {
 			size: sizes[index % sizes.length],
 			post: post
