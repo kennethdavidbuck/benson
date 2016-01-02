582333d71a385d1c121de44bcd09dd07ff66f935
Kenneth Buck <kenneth@nestedsphere.com>
2016-01-02 17:14:44 -0500

    changed dummy post data to be just 1

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -3,5 +3,5 @@ export default function (server) {
 	// Seed your development database using your factories. This
 	// data will not be loaded in your tests.
 
-	server.createList('post', 28);
+	server.createList('post', 1);
 }
