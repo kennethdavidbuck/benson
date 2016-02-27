3fb7a991c16b9ab70dabdce1e4f587a21a6c0fe1
Kenneth Buck <kenneth@nestedsphere.com>
2016-02-27 17:23:05 -0500

    added acceptance test demonstrating that correct number of articles are displayed on index

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -4,9 +4,15 @@ import moduleForAcceptance from 'benson/tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | index');
 
 test('visiting /', function (assert) {
+	assert.expect(2);
+
+	server.createList('post', 5);
+
 	visit('/');
 
 	andThen(function () {
 		assert.equal(currentURL(), '/');
+
+		assert.strictEqual(find('.page-content article').length, 5, 'There should be 5 articles displayed');
 	});
 });
