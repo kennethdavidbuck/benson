6ad5c5bc7b40dfb46a91758468768e34670eaba0
Kenneth Buck <kenneth@nestedsphere.com>
2016-02-28 14:12:41 -0500

    created default first post using same content as right rail

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -11,7 +11,50 @@ export default function (server) {
 
 	server.create('post', {
 		title: 'My First Post',
-		teaser: '<p>A Teaser</p>',
-		body: '<p>Hello World!</p>'
+		teaser: `<p>
+			Kenneth is a recovering <a href="http://www.joelonsoftware.com/articles/fog0000000018.html"
+			target="_blank">architect astronaut</a>,
+			based out of Toronto, Ontario. Back on earth, he spends his recovery time playing foosball, drinking
+			coffee, and browsing hacker news.
+			</p>
+			<p>
+			He finished his first degree in Archaeology in 2008 only to realize
+			how much he loved computers. He knew he had "no choice" but to head back to school
+			for a second degree in Computer Science. He has been helplessly addicted
+			to software and programming ever since.
+			</p>`,
+		body: `<p>
+			Kenneth is a recovering <a href="http://www.joelonsoftware.com/articles/fog0000000018.html"
+			target="_blank">architect astronaut</a>,
+			based out of Toronto, Ontario. Back on earth, he spends his recovery time playing foosball, drinking
+			coffee, and browsing hacker news.
+			</p>
+
+			<p>
+			He finished his first degree in Archaeology in 2008 only to realize
+			how much he loved computers. He knew he had "no choice" but to head back to school
+			for a second degree in Computer Science. He has been helplessly addicted
+			to software and programming ever since.
+			</p>
+
+			<p>
+			While in school he paired up with his former archaeology department to produce different works
+			including <a href="http://www.arcticiq.com" target="_blank">ArcticIQ</a>, a web based application geared
+			towards
+			the preservation and transmission of Inuit word meaning, and <a href="http://www.fortconger.ca"
+			target="_blank">Fort Conger</a>,
+			a maquette that was successful in securing funding through VMC Canada. He also enjoyed plenty of time
+			at <a href="http://www.aquanode.com" target="_blank">Aquanode</a>, a small Calgary based dev shop.
+			</p>
+
+			<p>
+			In 2013, he leaped straight out of university and directly into
+			<a href="https://www.bridezillahq.com" target="_blank">Bridezillahq</a>, a startup geared towards
+			disrupting the wedding market.
+			He spent 14th months there as CTO before deciding that the wedding game wasn't his style. He is now on
+			the lookout
+			for his next big adventure. You can reach him at <a href="mailto:kenneth@kennethbuck.ca">kenneth@kennethbuck.ca</a>
+			for opportunities and inquiries.
+			</p>`
 	});
 }
