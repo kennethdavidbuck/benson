6716232b8cf5fd2f485c2487c0ad4ebdc0b47bad
Kenneth Buck <kenneth@nestedsphere.com>
2016-02-28 13:43:25 -0500

    created boilerplate first post

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -3,9 +3,15 @@ export default function (server) {
 	// Seed your development database using your factories. This
 	// data will not be loaded in your tests.
 
-	var categories = server.createList('category', 5);
+	//var categories = server.createList('category', 5);
+  //
+	//server.createList('post', 5).forEach(function (post, index) {
+	//	post.categories = [categories[index]]
+	//});
 
-	server.createList('post', 5).forEach(function (post, index) {
-		post.categories = [categories[index]]
+	server.create('post', {
+		title: 'My First Post',
+		teaser: '<p>A Teaser</p>',
+		body: '<p>Hello World!</p>'
 	});
 }
